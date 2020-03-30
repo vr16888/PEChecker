92362b1585807f1d5a85018dfff13ee5eb7e74cc
unknown <ori.444@hotmail.com>
2020-03-30 15:29:01 +0300

    finished coff header

--- src/headers/COFFHeader.groovy
+++ src/headers/COFFHeader.groovy
@@ -1,4 +1,46 @@
 package headers
 
+import java.sql.Timestamp
+
 class COFFHeader {
+    private static final int HEADER_LENGTH = 20
+
+    /** The CPU that this file is intended for */
+    public final short Machine
+
+    /** The number of sections in the file. */
+    public final short NumberOfSections
+
+    /**
+     * The time that the linker (or compiler for an OBJ file) produced this file. This field holds the number of seconds since December
+     * 31st, 1969, at 4:00 P.M. (PST)
+     */
+    public final Timestamp TimeDateStamp
+
+    /**
+     * The file offset of the COFF symbol table. This field is only used in OBJ files and PE files with COFF debug information. PE files
+     * support multiple debug formats, so debuggers should refer to the IMAGE_DIRECTORY_ENTRY_DEBUG entry in the data directory (defined
+     * later).
+     */
+    public final int PointerToSymbolTable
+
+    /** The number of symbols in the COFF symbol table. See above. */
+    public final int NumberOfSymbols
+
+    /**
+     * The size of an optional header that can follow this structure. In OBJs, the field is 0. In executables, it is the size of the
+     * IMAGE_OPTIONAL_HEADER structure that follows this structure.
+     */
+    public final short SizeOfOptionalHeader
+
+    public COFFHeader(ByteArrayInputStream bytes) {
+        DataInputStream dataInputStream = new DataInputStream(bytes)
+
+        Machine = dataInputStream.readShort()
+        NumberOfSections = dataInputStream.readShort()
+        TimeDateStamp = dataInputStream.readInt()
+        PointerToSymbolTable = dataInputStream.readInt()
+        NumberOfSymbols = dataInputStream.readInt()
+        SizeOfOptionalHeader = dataInputStream.readInt()
+    }
 }
